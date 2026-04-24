c8745a9e483e1468e09ee9ce4704eac3e1fce0cd
glglak <karem.kamel@gmail.com>
2026-04-25 02:07:12 +0300

    feat: self-host IBM Plex Sans Arabic, Inter, JetBrains Mono

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+import "@fontsource/ibm-plex-sans-arabic/400.css";
+import "@fontsource/ibm-plex-sans-arabic/500.css";
+import "@fontsource/ibm-plex-sans-arabic/600.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/jetbrains-mono/400.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
